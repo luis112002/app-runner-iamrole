2ef9738e6d2fe0dab9cd12d9636414f622a625b1
luis112002 <148033715+luis112002@users.noreply.github.com>
2025-04-03 20:31:19 -0500

    Fix command

--- app.py
+++ app.py
@@ -1,52 +1,22 @@
-# from fastapi import FastAPI
-# import boto3
-
-# app = FastAPI()
-# s3 = boto3.client('s3')
-# bucket = "app-runner-entrada-luis"
-
-# @app.get("/")
-# def read_file():
-#     input_key = "entrada/mensaje.txt"
-#     output_key = "salida/resultado.txt"
-
-#     # Obtener contenido
-#     response = s3.get_object(Bucket=bucket, Key=input_key)
-#     content = response['Body'].read().decode('utf-8')
-
-#     # Crear salida
-#     resultado = content + "\nProcesado correctamente por FastAPI."
-#     s3.put_object(Bucket=bucket, Key=output_key, Body=resultado)
-
-#     return {"message": "¡Procesado con éxito!"}
-
-
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 import boto3
 
 app = FastAPI()
 s3 = boto3.client('s3')
 bucket = "app-runner-entrada-luis"
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def read_file(request: Request):
+@app.get("/")
+def read_file():
     input_key = "entrada/mensaje.txt"
     output_key = "salida/resultado.txt"
 
-    # Leer archivo desde S3
+    # Obtener contenido
     response = s3.get_object(Bucket=bucket, Key=input_key)
     content = response['Body'].read().decode('utf-8')
 
-    # Procesar y guardar
+    # Crear salida
     resultado = content + "\nProcesado correctamente por FastAPI."
     s3.put_object(Bucket=bucket, Key=output_key, Body=resultado)
 
-    # URL pública del archivo
-    s3_url = f"https://{bucket}.s3.us-east-2.amazonaws.com/{output_key}"
-
-    # Renderizar HTML
-    return templates.TemplateResponse("resultado.html", {"request": request, "s3_url": s3_url})
+    return {"message": "¡Procesado con éxito!"}
 
